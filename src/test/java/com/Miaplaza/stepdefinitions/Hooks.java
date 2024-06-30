d79ed3457396657bdfa4924f1c8ccec6c33ca556
selimoz <y.selimoz@hotmail.com>
2024-06-30 22:09:00 +0100

    closeDriver commented

--- src/test/java/com/Miaplaza/stepdefinitions/Hooks.java
+++ src/test/java/com/Miaplaza/stepdefinitions/Hooks.java
@@ -36,7 +36,7 @@ public class Hooks {
             scenario.attach(screenshot, "image/png", scenario.getName() + "_screenshot");
            // scenario.embed(screenshot, "image/png");
         }
-//        Driver.closeDriver();
+        Driver.closeDriver();
     }
 
     @After("@database")
